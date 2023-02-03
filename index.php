f2875c3cf39588a86c868a580e5d8a748047ce32
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-03 15:14:36 +0100

    piccole aggiunte e modifiche grafiche

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once __DIR__ . '/models/Movie.php';
+require_once __DIR__ . '/models/Movie.php';
 
 ?>
 
@@ -12,38 +12,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP</title>
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 
 <body>
     <?php
 
-        $ritornoAlFuturo = new Movie('ritorno al futuro',150,['fantasy','comico']);
-        $ritornoAlFuturo2 = new Movie('ritorno al futuro 2', 110, ['fantasy', 'comico','fantascientifico']);
+    $ritornoAlFuturo = new Movie('ritorno al futuro', 150, ['fantasy', 'comico']);
+    $ritornoAlFuturo2 = new Movie('ritorno al futuro 2', 110, ['fantasy', 'comico', 'fantascientifico']);
 
     ?>
-    <pre>
-        <?php
-            echo "\n";
-            echo 'titolo: ' . $ritornoAlFuturo->titolo;
-            echo "\n";
-            echo 'durata: ' . $ritornoAlFuturo->durata . " minuti" . ", " . $ritornoAlFuturo->durata();
-            echo "\n";
-            echo 'genere: ';
-            foreach($ritornoAlFuturo->genere as $element){
+    <main>
+        <h2>Film:</h2>
+        <div>
+            <?php
+            echo "<ul>";
+            echo '<li> <span>titolo:</span> ' . $ritornoAlFuturo->titolo . '</li>';
+            echo '<li> <span>durata:</span> ' . $ritornoAlFuturo->durata . " minuti" . ", " . $ritornoAlFuturo->durata() . '</li>';
+            echo '<li> <span>genere:</span> ';
+            foreach ($ritornoAlFuturo->genere as $element) {
                 echo $element . " ";
             }
-            echo '<br>';
-            echo "\n";
-            echo 'titolo: ' .  $ritornoAlFuturo2->titolo;
-            echo "\n";
-            echo 'durata: ' . $ritornoAlFuturo2->durata . " minuti" . ", " . $ritornoAlFuturo2->durata();
-            echo "\n";
-            echo 'genere: ';
+            echo '</li>';
+            echo '</ul>';
+            ?>
+        </div>
+        <div>
+            <?php
+            echo "<ul>";
+            echo '<li> <span>titolo:</span> ' .  $ritornoAlFuturo2->titolo . '</li>';
+            echo '<li> <span>durata:</span> ' . $ritornoAlFuturo2->durata . " minuti" . ", " . $ritornoAlFuturo2->durata() . '</li>';
+            echo '<li> <span>genere:</span> ';
             foreach ($ritornoAlFuturo2->genere as $element) {
                 echo $element . " ";
             }
-        ?>
-    </pre>
+            echo '</li>';
+            echo '</ul>';
+            ?>
+        </div>
+    </main>
 
 </body>
 
